de53cafa23e3236ed73eba244ad6399df395b3ca
Kumag8 <75565726+Kumag8@users.noreply.github.com>
2020-12-17 19:18:48 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+let Direct = 0
+let Speed = 0
+radio.setGroup(88)
+basic.forever(function () {
+    Speed = pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    -7,
+    8
+    )
+    if (Speed >= 7) {
+        radio.sendString("前7")
+    } else if (Speed == 6) {
+        radio.sendString("前6")
+    } else if (Speed == 5) {
+        radio.sendString("前5")
+    } else if (Speed == 4) {
+        radio.sendString("前4")
+    } else if (Speed == 3) {
+        radio.sendString("前3")
+    } else if (Speed == 2) {
+        radio.sendString("前2")
+    } else if (Speed == 1) {
+        radio.sendString("前1")
+    } else if (Speed == 0) {
+        radio.sendString("とまる")
+    } else if (Speed == -1) {
+        radio.sendString("後1")
+    } else if (Speed == -2) {
+        radio.sendString("後2")
+    } else if (Speed == -3) {
+        radio.sendString("後3")
+    } else if (Speed == -4) {
+        radio.sendString("後4")
+    } else if (Speed == -5) {
+        radio.sendString("後5")
+    } else if (Speed == -6) {
+        radio.sendString("後6")
+    } else if (Speed == -7) {
+        radio.sendString("後7")
+    }
+})
+basic.forever(function () {
+    Direct = pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    -3,
+    4
+    )
+    if (Direct >= 3) {
+        radio.sendString("右3")
+    } else if (Direct == 2) {
+        radio.sendString("右2")
+    } else if (Direct == 1) {
+        radio.sendString("右1")
+    } else if (Direct == 0) {
+        radio.sendString("かいてんおわり")
+    } else if (Direct == -1) {
+        radio.sendString("左1")
+    } else if (Direct == -2) {
+        radio.sendString("左2")
+    } else if (Direct == -3) {
+        radio.sendString("左3")
+    }
+})
